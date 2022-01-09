9316b411d312c8a27020115194c69899022977ec
junex0314 <92265279+junex0314@users.noreply.github.com>
2022-01-09 12:40:42 +0800

    Add files via upload

---
+++ ch12/pack14/class14.java
@@ -0,0 +1,11 @@
+package pack14;
+
+public class class14 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		String str = "Habit is second nature";
+		System.out.println( str.substring(15));
+	}
+
+}
---
+++ ch12/pack17/class17.java
@@ -0,0 +1,11 @@
+package pack17;
+
+public class class17 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		String str = "262904713";
+		System.out.println(Short.parseShort(str));
+	}
+
+}
