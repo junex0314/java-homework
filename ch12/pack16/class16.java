5e6196494c37055e25380d954b003e735971e30d
junex0314 <92265279+junex0314@users.noreply.github.com>
2022-01-09 12:28:48 +0800

    Add files via upload

---
+++ ch12/pack16/class16.java
@@ -0,0 +1,11 @@
+package pack16;
+
+public class class16 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		String str = "262904713";
+		System.out.println(Integer.parseInt(str));
+	}
+
+}
